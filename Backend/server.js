3d89d8598a5f74459ba541ce9a6c46d0cc284260
meet parmar <meetparmar2362004@gmail.com>
2025-07-25 12:04:45 +0530

    dployment

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,13 @@
   const PORT = process.env.PORT || 5000;
 
   // Middleware
-  app.use(cors());
+  app.use(cors(
+    {
+      origin: ["http://localhost:3000","https://my-to-do-app-blue.vercel.app/"], 
+      methods: "GET,POST,PUT,DELETE",
+      credentials: true,
+    }
+  ));
   app.use(express.json());
 
   // Routes
